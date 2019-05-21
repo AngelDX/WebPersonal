798466fdc2ad3d302145b22cf2378de0021421c5
davidmp <mamanipari@gmail.com>
2019-05-21 09:15:27 -0500

    personalizar admin

--- portafolio/admin.py
+++ portafolio/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Project
-# Register your models here.
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+	readonly_fields=('create','update')
+	list_display=('titulo','imagen','create','update')
+	ordering=('titulo','create')
+	search_fields=('titulo','descripcion')
+	date_hierarchy='create'
+	list_filter=('titulo','create')
+
+admin.site.register(Project,ProjectAdmin)
--- portafolio/models.py
+++ portafolio/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-	titulo=models.CharField(max_length=30)
+	titulo=models.CharField(max_length=30,verbose_name='Titulo del Proyecto')
 	descripcion=models.TextField()
 	imagen=models.ImageField(upload_to='proyectos')
-	link=models.URLField(null=True,blank=True)
+	link=models.URLField(null=True,blank=True,verbose_name='URL del proyecto')
 	create=models.DateTimeField(auto_now_add=True,verbose_name='Fecha de Creación')
 	update=models.DateTimeField(auto_now=True,verbose_name='Fecha de Actualización')
 
+	class Meta:
+		verbose_name='Proyecto'
+		verbose_name_plural='Mis Proyectos'
 
 	def __str__(self):
 		return self.titulo
